1eb7ab6499d6ad3f82ae579340c94c10e58880e4
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2017-03-05 10:54:44 +0000

    code for storing commplted

--- scrape.php
+++ scrape.php
@@ -3,22 +3,21 @@
     $user = "sanjay_ch";
     $pass = "3vXt73bGW7mEcGnI";
     
-                                  $conn = mysqli_connect($host,$user,$pass);
-    if (!$conn) {
+    $conn = mysqli_connect($host,$user,$pass);
+    if (!$conn) 
+    {
     die('Could not connect: ' . mysql_error());
     }
     $db = mysqli_select_db($conn,'project1');
     $url = $_POST["url"];
     $html = file_get_contents($url);
     preg_match_all('/(?<=blank">)(.*)<\/a>\n<p>\| (.*)<\/p><\/h2>/',$html,$coll_add);
-    var_dump($coll_add_revw);
-    preg_match_all('/facility-icons|(?<=<h3>).*(?=<\/h3>\n<p>)|(?<=<span><b>)(.*)(?=<\/b>)/',$html,$faclties_rev);
-    
+    preg_match_all('/facility-icons|(?<=<h3>).*(?=<\/h3>\n<p>)|(?<=<span><b>).*(?=<\/b>)/'
+        ,$html,$faclties_revw);
     $faclty_text = null;
     $i = 0;
     $count = 0;
-    $num = 0;
-    foreach ($faclties[0] as $faclty)
+    foreach ($faclties_revw[0] as $faclty)
     {
         if ($faclty != 'facility-icons')
         {
@@ -30,9 +29,17 @@
         }
         else if($faclty_text != null)
         {
+            if(is_numeric($faclties_revw[0][($count)-1]))
+            {
+                $num = $faclties_revw[0][($count)-1];
+            }
+            else
+            {
+                $num = 0;
+            }
             $faclty_text = rtrim($faclty_text,",");
-            mysqli_query($conn,"INSERT INTO project1  (college, location, facilities)
-                VALUES ('{$coll_add[1][$i]}', '{$coll_add[2][$i]}', '{$faclty_text}')");
+            mysqli_query($conn,"INSERT IGNORE INTO details (college, location, facilities, reviews)
+                VALUES ('{$coll_add[1][$i]}', '{$coll_add[2][$i]}', '{$faclty_text}', '{$num}')");
             $i++;
             
             $faclty_text = null;
