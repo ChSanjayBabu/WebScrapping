227f26616df9d881dabe93e8d3e1ff6a0f923286
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2017-03-05 17:23:46 +0000

    changes in index

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
-<?php
-    
-    $host = "localhost";
-    $user = "sanjay_ch";
-    $pass = "3vXt73bGW7mEcGnI";
-    
-    $conn = mysqli_connect($host,$user,$pass);
-    if (!$conn) {
-    die('Could not connect: ' . mysql_error());
-    }
-    $db = mysqli_select_db($conn,'project1');
 
-?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Scrapping siksha</title>
+
     </head>
+
     <body>
-        <form action="scrape.php" method="post">
-            <input type="text" name="url" placeholder="enter city url">
-            <br>
-            <input type="submit" value="search">
+        <script>
+        function func()
+        {
+            var x =  document.getElementsByName("url")[0].value;
+            document.getElementById("test1").innerHTML = x;
+
+        }
+        </script>
+
+        <p id="test1">   .</p>
+        <form onsubmit = "func(); return false" >
+            <input placeholder = "type url" type = "text" name="url">
+            <input type = "submit" value ="search">
         </form>
     </body>
 </html>
